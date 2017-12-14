9e6486336e81d0df5ed574bf135baf8f533f9973
Joapfel <kraemer_johannes@gmx.de>
2017-12-14 21:40:48 +0100

    creating exercises

--- src/de/ws1718/ismla/verbtrainer/client/SpanishVerbtrainer.java
+++ src/de/ws1718/ismla/verbtrainer/client/SpanishVerbtrainer.java
@@ -4,6 +4,7 @@ import de.ws1718.ismla.verbtrainer.shared.FieldVerifier;
 import de.ws1718.ismla.verbtrainer.shared.TokenExercise;
 import javafx.beans.binding.StringBinding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.google.gwt.core.client.EntryPoint;
@@ -18,8 +19,11 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.InlineHTML;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextArea;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
@@ -27,133 +31,68 @@ import com.google.gwt.user.client.ui.VerticalPanel;
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class SpanishVerbtrainer implements EntryPoint {
-	/**
-	 * The message displayed to the user when the server cannot be reached or
-	 * returns an error.
-	 */
-	private static final String SERVER_ERROR = "An error occurred while "
-			+ "attempting to contact the server. Please check your network " + "connection and try again.";
 
 	/**
 	 * Create a remote service proxy to talk to the server-side Greeting service.
 	 */
-	private final GreetingServiceAsync greetingService = GWT.create(GreetingService.class);
+	private final GreetingServiceAsync client2Server = GWT.create(GreetingService.class);
 
 	/**
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
+		
+		final TextArea textArea = new TextArea();
+		
+		final HTMLPanel exerciseContainer = new HTMLPanel("");
+		
 		final Button sendButton = new Button("Send");
-		final TextBox nameField = new TextBox();
-		nameField.setText("GWT User");
-		final Label errorLabel = new Label();
-
-		// We can add style names to widgets
 		sendButton.addStyleName("sendButton");
-
-		// Add the nameField and sendButton to the RootPanel
-		// Use RootPanel.get() to get the entire body element
-		RootPanel.get("nameFieldContainer").add(nameField);
-		RootPanel.get("sendButtonContainer").add(sendButton);
-		RootPanel.get("errorLabelContainer").add(errorLabel);
-
-		// Focus the cursor on the name field when the app loads
-		nameField.setFocus(true);
-		nameField.selectAll();
-
-		// Create the popup dialog box
-		final DialogBox dialogBox = new DialogBox();
-		dialogBox.setText("Remote Procedure Call");
-		dialogBox.setAnimationEnabled(true);
-		final Button closeButton = new Button("Close");
-		// We can set the id of a widget by accessing its Element
-		closeButton.getElement().setId("closeButton");
-		final Label textToServerLabel = new Label();
-		final HTML serverResponseLabel = new HTML();
-		VerticalPanel dialogVPanel = new VerticalPanel();
-		dialogVPanel.addStyleName("dialogVPanel");
-		dialogVPanel.add(new HTML("<b>Sending name to the server:</b>"));
-		dialogVPanel.add(textToServerLabel);
-		dialogVPanel.add(new HTML("<br><b>Server replies:</b>"));
-		dialogVPanel.add(serverResponseLabel);
-		dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
-		dialogVPanel.add(closeButton);
-		dialogBox.setWidget(dialogVPanel);
-
-		// Add a handler to close the DialogBox
-		closeButton.addClickHandler(new ClickHandler() {
+		sendButton.addClickHandler(new ClickHandler() {
+			
+			@Override
 			public void onClick(ClickEvent event) {
-				dialogBox.hide();
-				sendButton.setEnabled(true);
-				sendButton.setFocus(true);
-			}
-		});
-
-		// Create a handler for the sendButton and nameField
-		class MyHandler implements ClickHandler, KeyUpHandler {
-			/**
-			 * Fired when the user clicks on the sendButton.
-			 */
-			public void onClick(ClickEvent event) {
-				sendNameToServer();
-			}
-
-			/**
-			 * Fired when the user types in the nameField.
-			 */
-			public void onKeyUp(KeyUpEvent event) {
-				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-					sendNameToServer();
-				}
-			}
-
-			/**
-			 * Send the name from the nameField to the server and wait for a response.
-			 */
-			private void sendNameToServer() {
-				// First, we validate the input.
-				errorLabel.setText("");
-				String textToServer = nameField.getText();
-				if (!FieldVerifier.isValidName(textToServer)) {
-					errorLabel.setText("Please enter at least four characters");
-					return;
-				}
-
-				// Then, we send the input to the server.
-				sendButton.setEnabled(false);
-				textToServerLabel.setText(textToServer);
-				serverResponseLabel.setText("");
-//				greetingService.greetServer(textToServer, new AsyncCallback<String>() {
-//					public void onFailure(Throwable caught) {
-//						// Show the RPC error message to the user
-//						dialogBox.setText("Remote Procedure Call - Failure");
-//						serverResponseLabel.addStyleName("serverResponseLabelError");
-//						serverResponseLabel.setHTML(SERVER_ERROR);
-//						dialogBox.center();
-//						closeButton.setFocus(true);
-//					}
-//
-//					public void onSuccess(String result) {
-//						dialogBox.setText("Remote Procedure Call");
-//						serverResponseLabel.removeStyleName("serverResponseLabelError");
-//						serverResponseLabel.setHTML(result);
-//						dialogBox.center();
-//						closeButton.setFocus(true);
-//					}
-//				});
 				
-				greetingService.getTokensForExercise(textToServer, new AsyncCallback<List<TokenExercise>>() {
-					
+				String input = textArea.getText();
+				
+				client2Server.getTokensForExercise(input, new AsyncCallback<List<TokenExercise>>() {
 					@Override
 					public void onSuccess(List<TokenExercise> result) {
 						// TODO Auto-generated method stub
-						Window.alert("SUCCESS");
-						StringBuilder sb = new StringBuilder();
-						for(TokenExercise tokenEx : result){
+//						Window.alert("SUCCESS");
+
+						List<TextBox> submissions = new ArrayList<>();
+						
+						//create exercises
+						for(TokenExercise tokenEx : result){	
 							
-							sb.append(tokenEx.getToken() +  " -> " + tokenEx.isVerb() + "\n");
+							if(tokenEx.isVerb()){
+								TextBox tb = new TextBox();
+								submissions.add(tb);
+								exerciseContainer.add(tb);
+								exerciseContainer.add(new InlineHTML("<span id='token_hint'>(" + tokenEx.getToken() + ")</span>"));
+							}else{
+								exerciseContainer.add(new InlineHTML("<span id='token'> " + tokenEx.getToken() + " </span>"));
+							}
 						}
-						Window.alert(sb.toString());
+						
+						//create submit button
+						Button submitButton = new Button("Submit");
+						submitButton.addStyleName("submitButton");
+						submitButton.addClickHandler(new ClickHandler() {
+							
+							@Override
+							public void onClick(ClickEvent event) {
+								// TODO Auto-generated method stub
+								StringBuilder sb = new StringBuilder();
+								for(TextBox tb : submissions){
+									sb.append(tb.getText());
+								}
+								Window.alert(sb.toString());
+							}
+						});
+						
+						RootPanel.get("exercise_view").add(submitButton);
 					}
 					
 					@Override
@@ -162,12 +101,16 @@ public class SpanishVerbtrainer implements EntryPoint {
 						Window.alert("FAILED");
 					}
 				});
+				
 			}
-		}
+		});
+		
+		
 
-		// Add a handler to send the name to the server
-		MyHandler handler = new MyHandler();
-		sendButton.addClickHandler(handler);
-		nameField.addKeyUpHandler(handler);
+		RootPanel.get("textAreaContainer").add(textArea);
+		RootPanel.get("textAreaContainer").add(sendButton);
+		RootPanel.get("exercise_view").add(exerciseContainer);
+
+
 	}
 }
